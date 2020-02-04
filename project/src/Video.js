5084f73bcd123a0df446702f2a4a22f48096df5d
Unknown <mael.ortiz@gmail.com>
2020-02-04 10:16:10 +0100

    Video

---
+++ project/src/Video.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
+
+class Video extends Component {
+  render () {
+    return <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
+  }
+}
+
+export default Video;
+
+
+ 
